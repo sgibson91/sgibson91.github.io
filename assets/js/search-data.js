700982c9d217336ba60e43e7babc65f1a717dcc4
sgibson91 <sgibson91@users.noreply.github.com>
2021-01-29 12:08:27 +0000

    Deploying to gh-pages from  @ 6ceca1acf7534f8b0ed048c98a73a902f2734404 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,43 +1,41 @@
----
-layout: null
-sitemap: false
----
-
-var store = [
-  {%- for c in site.collections -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-    {%- assign docs = c.docs | where_exp: 'doc', 'doc.search != false' -%}
-    {%- for doc in docs -%}
-      {
-        "title": {{ doc.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-          {{ doc.content | 
-            replace:"</p>", " " | 
-            replace:"</h1>", " " | 
-            replace:"</h2>", " " | 
-            replace:"</h3>", " " | 
-            replace:"</h4>", " " | 
-            replace:"</h5>", " " | 
-            replace:"</h6>", " "|
-          strip_html | strip_newlines | jsonify }},
-        {%- else -%}
-          {{ doc.content | 
-            replace:"</p>", " " | 
-            replace:"</h1>", " " | 
-            replace:"</h2>", " " | 
-            replace:"</h3>", " " | 
-            replace:"</h4>", " " | 
-            replace:"</h5>", " " | 
-            replace:"</h6>", " "|
-           strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-        {%- endif -%}
-        "categories": {{ doc.categories | jsonify }},
-        "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | absolute_url | jsonify }}
-      } {%- unless forloop.last and l -%}, {%- endunless -%}
-    {%- endfor -%}
-  {%- endfor -%}
-]
+var store = [{
+        "title": "Diving into Leadership to Build Push-Button Code",
+        "excerpt":"“Hi everyone, I’m Sarah! I’m a Research Data Scientist at the Alan Turing Institute and I’m also an operator of mybinder.org. It’s really cool seeing how many people here are interested in BinderHub!” And it is cool. Really cool! But also a bit scary as a room full of Research...","categories": ["blog"],
+        "tags": ["hackathon","sprint","binder","binderhub","leadership","microsoft","azure","cloud"],
+        "url": "https://sgibson91.github.io/blog/diving-into-leadership/"
+      },{
+        "title": "Binder",
+        "excerpt":"Binder is a project that allows users to create custom computing environments that can be shared and used by many remote users. It is powered by BinderHub, which is an open-source tool that deploys the Binder service in the cloud. One such Binder service is mybinder.org and is free to...","categories": ["projects"],
+        "tags": ["reproducibility","cloud","community","open-source"],
+        "url": "https://sgibson91.github.io/projects/binder/"
+      },{
+        "title": "HelmUpgradeBot",
+        "excerpt":"While managing BinderHubs, I found that there were a lot of things I had to deal with manually and, inevitably, I failed to keep on top of them.Most notably, the two biggest problems I faced were i) keeping my BinderHub up-to-date with the latest software released by the Binder team,...","categories": ["projects"],
+        "tags": ["reproducibility","binderhub","automation"],
+        "url": "https://sgibson91.github.io/projects/helm-upgrade-bot/"
+      },{
+        "title": "The Turing Way",
+        "excerpt":"The Turing Way is an online handbook - and global community - dedicated to fostering gold-standard reproducible research. It’s a cultural movement with the potential to transform data science. There is a crisis of reproducibility in science. ‘Publish-or-perish’ incentives and excessive data secrecy stifles progress and wastes resources: siloed science...","categories": ["projects"],
+        "tags": ["reproducibility","data-science","community"],
+        "url": "https://sgibson91.github.io/projects/the-turing-way/"
+      },{
+        "title": "2019: Seasons of Reflection",
+        "excerpt":"As 2019 comes to a close, I have spent a full year as a Research Software Engineer (RSE) at The Alan Turing Institute.A lot has changed over the last 12 months and I’ve come a long way - sometimes surprising myself! Winter :snowflake: In February, I gave my first professional...","categories": ["blog"],
+        "tags": ["reflection","growth"],
+        "url": "https://sgibson91.github.io/blog/seasons-of-reflection/"
+      },{
+        "title": "Software Sustainability Fellowship 2020",
+        "excerpt":"So 2020 is off to an incredibly exciting start! I have been awarded a Software Sustainability Institute 2020 Fellowship!!! :tada: :tada: :tada: We are thrilled to announce the new cohort of @SoftwareSaved Fellows for 2020! You can find out more about the new cohort here: https://t.co/WPTePa3hoPCongratulations to the SSI Fellows...","categories": ["blog"],
+        "tags": ["announcement","community"],
+        "url": "https://sgibson91.github.io/blog/ssi-fellowship/"
+      },{
+        "title": "February 2020 Update",
+        "excerpt":"Hello friends! :wave:It feels like such a long time since I wrote a blog post but the truth is that I’ve just been up to so many exciting things!So this blog post will be a quick run down on everything I’ve been up to recently. At the start of January,...","categories": ["blog"],
+        "tags": [],
+        "url": "https://sgibson91.github.io/blog/february-2020-update/"
+      },{
+        "title": "Continuous Integration: Fail Fast and Fail First",
+        "excerpt":"Sarah and Graham have different career backgrounds - Sarah having come through academia whereas Graham earned his stripes in industry. However in their current roles, they often find themselves using the same tools, for example Continuous Integration. They have written this blog post to identify how academia and industry may...","categories": ["blog"],
+        "tags": ["continuous-integration","tools","devops"],
+        "url": "https://sgibson91.github.io/blog/continuous-integration-fail-fast-fail-first/"
+      }]
